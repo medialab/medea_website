19da1746d2dff91e9c76bb700f1cbf18915c17bd
Daniele Guido <gui.daniele@gmail.com>
2014-11-27 14:00:09 +0100

    different template for main page and various subproject pages

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -20,7 +20,7 @@ angular
   .config(function ($routeProvider) {
     $routeProvider
       .when('/', {
-        templateUrl: 'views/page.html',
+        templateUrl: 'views/main.html',
         controller: 'MainCtrl',
         resolve: {
           content : function ($route, FilesFactory) {
