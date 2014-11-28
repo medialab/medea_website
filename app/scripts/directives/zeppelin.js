c6102691c8fe0a3f9f7ad7b18e08f38cd2af83ed
Daniele Guido <gui.daniele@gmail.com>
2014-11-28 15:04:06 +0100

    zeppelin sending signals

--- app/scripts/directives/zeppelin.js
+++ app/scripts/directives/zeppelin.js
@@ -14,67 +14,75 @@ angular.module('driveoutApp')
       scope:{
         sections: '='
       },
-      link: function postLink(scope, element, attrs) {
-        $log.debug("\n\nZEPPELIN - the magic directive");
+      controller: function($scope, $element, $attrs, $log) {
         var w = angular.element($window);
-        element.on('click', '[data-click]', function(e) {
-          $log.info('clicked on ', angular.element(e).data('data-click'), arguments)
-        })
-        scope.h = 0;
-        
-        console.log();
 
-        scope.index  = 0; // current section index, cfr params otherwise
+        $log.debug('go zeeppelin go', $scope.sections);
+        $scope.index = 0;
+        $scope.height = w.height() - 250;
 
-        // goto next slide
-        scope.next = function() {
-          var _index = scope.index + 1;
-          scope.index = Math.min(_index, scope.sections.length -1);
-          $log.info('next slide', scope.index, '/', scope.sections.length - 1, -scope.index * scope.h);
-          scope.steer(scope.index);
+        $scope.next = function() {
+          var _index = $scope.index + 1;
+          $scope.index = Math.min(_index, $scope.sections.length -1);
+          $log.info('next slide', $scope.index, '/', $scope.sections.length - 1, -$scope.index * $scope.height);
+          $scope.steer($scope.index);
         };
         
         // goto previous slide
-        scope.previous = function() {
-          var _index = scope.index - 1;
-          scope.index = Math.max(_index, 0);
-          $log.info('next slide', scope.index, '/', scope.sections.length - 1);
-          scope.steer(scope.index);
+        $scope.previous = function() {
+          var _index = $scope.index - 1;
+          $scope.index = Math.max(_index, 0);
+          $log.info('next slide', $scope.index, '/', $scope.sections.length - 1);
+          $scope.steer($scope.index);
         };
 
+        
         /*
           Goto a specific slide. If the section directive names has the 'follow' prefix,
           we do not substitute the current section in playground, but
           we 'inject' preliminary params instead. This function handle the 
           the section scrolling inside the slider as well.
         */
-        scope.steer = function(index) {
-          element.find('.slider').css({
-            top: -scope.index * scope.h
-          });
-          if(scope.sections[scope.index]){
-            scope.section = scope.sections[scope.index];
+        $scope.steer = function(index){
+          if($scope.sections[$scope.index]){
+            $scope.section = $scope.sections[$scope.index];
           }
         };
 
-        
         /*
           Once timed properly, this funciton resize the szeppelin working area
           (fixing the height porperty according to window height props)
         */
-        function resize() {
-          scope.h = (w.height() - 250);
-          console.log('/ zeppelin ---> ',scope.h)
-          element.css({
-            height: scope.h
-          })
+        w.on('resize', function () {
+          $scope.height = w.height() - 250;
+          $scope.$apply()
+        });
+        
+      },
+      link: function postLink(scope, element, attrs) {
+        $log.debug("\n\nZEPPELIN - the magic directive");
+        element.on('click', '[data-click]', function(e) {
+          $log.info('clicked on ', $(this).attr('data-click'), arguments)
+          scope.$broadcast('focus', '' + $(this).attr('data-click'));
+        })
+        
+        // goto next slide
+        scope.$watch('index', function(index) {
+          $log.info('index changed', scope.height, element.find('.slider'))
           element.find('.slider').css({
-            top: -scope.index * scope.h
+            top: -index * scope.height
           });
-        };
+        });
 
-        w.bind('resize', resize);
-        resize();
+        scope.$watch('height', function(h) {
+          $log.info('height changed', h)
+           element.css({
+            height: h
+          })
+        });
+        
+
+        
       }
     };
   })
