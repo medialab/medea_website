0ef3f76869536a3cb0161007d8c79414cbafffd9
Pierre <pierre.legault@sciencespo.fr>
2015-05-05 15:29:13 +0200

    Bricks viz now integrated

---
+++ app/scripts/directives/bricks-partByChapters.js
@@ -0,0 +1,32 @@
+'use strict';
+
+/**
+ * @ngdoc directive
+ * @name driveoutApp.directive:participationByChapters
+ * @description
+ * # bricks diagram
+ */
+angular.module('driveoutApp')
+  .directive('participationbychapters', function () {
+    return {
+      restrict: 'EA',
+      scope: {},
+      link: function postLink(scope, element, attrs) {
+        var elementwidth = element.width() * 9.8/10;
+        var bricks = new Bricks();
+        bricks.load('contents/data/bricks-partByChapters/participations.json', function() {
+          var width = elementwidth,
+              height = window.innerHeight/1.5,
+              margin = {top: 40, bottom: 70, left: 40, right: 40};
+
+          bricks.drawViz('#bricksSvgContainer',
+            {
+              height: height,
+              width: width,
+              vizName: 'viviz',
+              margin: margin
+            });
+      });
+    }
+  };
+});
--- app/scripts/directives/ipcc-people-lines.js
+++ app/scripts/directives/ipcc-people-lines.js
@@ -12,7 +12,6 @@ angular.module('driveoutApp')
       restrict: 'EA',
       scope: {},
       link: function postLink(scope, element, attrs) {
-
         var elementwidth = element.width() * 9.8/10,
             opts = '',
             select = document.querySelector('#lines-countries-choice'),
@@ -20,13 +19,13 @@ angular.module('driveoutApp')
             viz = new IPCCPeopleLines();
 
         viz.load_countries('contents/data/people-countries/countries.json', function() {
-  
+
           // populate dropdown list of countries
           viz.listCountries.forEach(function(c) {
             opts += '<option ' + (c === viz.defaultCountry ? 'selected="selected" ' : '') + 'value="' + c + '">' + c + '</option>\n';
           });
           select.innerHTML = opts;
-  
+
           select.onchange = sort.onchange = function(e) {
             viz.load_country(select.value, "contents//data/people-countries", function() {
               viz.draw_country("#lines-countries-viz", select.value, sort.value, elementwidth);
