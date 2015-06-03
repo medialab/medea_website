38e9bc4b1416ffb69d81ab46eee6a685697282c0
Pierre <pierre.legault@sciencespo.fr>
2015-06-03 16:55:10 +0200

    Title added to participationRatesHisto

--- app/scripts/directives/participationRatesHisto.js
+++ app/scripts/directives/participationRatesHisto.js
@@ -17,7 +17,7 @@ angular.module('driveoutApp')
         var histogram = new PowerLawHistogram();
         histogram.load('contents/data/participationRatesHisto/participations.json', function() {
           var width = elementWidth,
-              height = elementHeight/2,
+              height = elementHeight/2.2,
               margin = {top: 40, bottom: 70, left: 40, right: 40};
 
           histogram.drawChart('#containerNonBridge',
