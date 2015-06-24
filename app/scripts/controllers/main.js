13d532f8d700aeede28c634e9c799682ab8d8e7a
Pierre <pierre.legault@sciencespo.fr>
2015-06-24 12:44:43 +0200

    Main page structure now OK

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,7 +1,9 @@
 angular.module('driveoutApp.main', [])
-  .controller('MainCtrl', function ($scope, content) {
+  .controller('MainCtrl', function ($scope, $location, content) {
     console.log(content);
     var retrievedContent = content.data;
+    var studyPath = $location.path().replace(/\/\w*$/, '');
+    retrievedContent.studyPath = studyPath;
     $scope.studyIntro = retrievedContent;
     console.log($scope)
   });
