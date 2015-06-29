b169c0882a2bb0dcfa258b314e0053b6c2d14204
Pierre <pierre.legault@sciencespo.fr>
2015-06-29 17:31:21 +0200

    Story line upper zone WIP

--- app/scripts/controllers/page.js
+++ app/scripts/controllers/page.js
@@ -8,7 +8,10 @@
  * Controller of the driveoutApp
  */
 angular.module('driveoutApp.page', [])
-  .controller('PageCtrl', function ($scope, content, $routeParams) {
+  .controller('PageCtrl', function ($scope, content, $location, $routeParams) {
     $scope.content = content.data;
+    var studyPath = $location.path().replace(/\/[^\/]*$/, '');
+    console.log(studyPath);
+    $scope.content.studyPath = studyPath;
     $scope.tab = $routeParams.sub || 'introduction';
   });
