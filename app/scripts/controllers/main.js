61417e2e317c9fae5fa3487c28e06d724cf317cc
Pierre <pierre.legault@sciencespo.fr>
2015-07-03 15:16:32 +0200

    Main view removed

--- app/scripts/controllers/main.js
+++
@@ -1,8 +0,0 @@
-angular.module('driveoutApp.main', [])
-  .controller('MainCtrl', function ($scope, $location, $http, content) {
-    $('html, body').scrollTop(0);
-    var retrievedContent = content.data;
-    var studyPath = $location.path().replace(/\/\w*$/, '');
-    retrievedContent.studyPath = studyPath;
-    $scope.studyIntro = retrievedContent;
-  });
