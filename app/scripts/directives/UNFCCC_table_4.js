abeb9371abed3f0c08ea9381e6315771b095c835
Pierre <pierre.legault@sciencespo.fr>
2015-07-16 19:19:38 +0200

    All viz added to introduction

--- app/scripts/directives/UNFCCC_table_4.js
+++ app/scripts/directives/UNFCCC_table_4.js
@@ -3,6 +3,36 @@
 angular.module('driveoutApp.directives.unfccctablefour', [])
   .directive('unfccctablefour', function () {
     return {
-      templateUrl: 'views/templates/UNFCCC_table_4.html'
+      templateUrl: 'views/templates/UNFCCC_table_4.html',
+      link: function postLink(scope, element) {
+        var titleTable1 = 'List of the automatically extracted terms with their different forms',
+            titleTable2 = 'List of the manually selected terms with their forms and frequency';
+
+        $('#extractedTermsButton').on('click', function() {
+          $('#tableExtracted').removeClass('displayNone');
+
+          if (!$('#tableSelected').hasClass('displayNone'))
+            $('#tableSelected').addClass('displayNone');
+
+          $('#extractedTermsButton').prop('disabled', true)
+          $('#selectedTermsButton').prop('disabled', false)
+
+          if ($('.vizLegendText h4').text() !== titleTable1)
+            $('.vizLegendText h4').text(titleTable1);
+        });
+
+        $('#selectedTermsButton').on('click', function() {
+          $('#tableSelected').removeClass('displayNone');
+
+          $('#extractedTermsButton').prop('disabled', false)
+          $('#selectedTermsButton').prop('disabled', true)
+
+          if (!$('#tableExtracted').hasClass('displayNone'))
+            $('#tableExtracted').addClass('displayNone');
+
+          if ($('.vizLegendText h4').text() !== titleTable2)
+            $('.vizLegendText h4').text(titleTable2)
+        });
+      }
     };
   });
